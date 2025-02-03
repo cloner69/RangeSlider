9ae38d6a11e71ba125c9b55173558aa958402788
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 16:43:06 +0700

    range slider

---
+++ RangeSlider/RangeSlider/RangeSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RangeSliderApp.swift
+//  RangeSlider
+//
+//  Created by Adrian Suryo Abiyoga on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RangeSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
